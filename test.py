7927bbe3ea9f3e24518770f21dbd5ef9ae0534e7
zhuangzhong <zhuangzhong@corp.netease.com>
2024-03-19 10:35:02 +0800

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from eetq.models import AutoEETQForCausalLM
-from transformers import AutoTokenizer
-
-model_name = "/data/backup/Llama-2-7b-chat-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-quant_path = "/data/Llama-2-7b-chat-eetq"
-model = AutoEETQForCausalLM.from_pretrained(model_name)
-model.quantize(quant_path)
-tokenizer.save_pretrained(quant_path)
